dbb88b0f74e816a3e60bbe7f8b4a1f59e8ad2cd0
Tabish Tanseef <tabishtanseef@gmail.com>
2019-08-13 12:00:42 +0530

    testing ebook

---
+++ data/config.js
@@ -0,0 +1,119 @@
+var fPage = 1;
+						 const appdb = {
+						'config': {
+							'subject': 'Lets Talk - 1',
+							'class': 'Class 6',
+							'id': 'gud_006',
+							'totalPages': 64,
+							'bookWidth': 2570,
+							'bookHeight': 3300
+						},
+						'ebook': {'toc': {
+							'icon': 'img/content.png',
+							'menu': 'Table of Contents',
+							'link': 'content',
+							'data': [{
+						'page': 5,
+						'title': 'Unit - 1'
+					},{
+						'page': 9,
+						'title': 'Unit - 2'
+					},{
+						'page': 14,
+						'title': 'Unit - 3'
+					},{
+						'page': 18,
+						'title': 'Unit - 4'
+					},{
+						'page': 23,
+						'title': 'Unit - 5'
+					},{
+						'page': 28,
+						'title': 'Unit - 6'
+					},{
+						'page': 33,
+						'title': 'Unit - 7'
+					},{
+						'page': 38,
+						'title': 'Unit - 8'
+					},{
+						'page': 43,
+						'title': 'Unit - 9'
+					},{
+						'page': 49,
+						'title': 'Unit - 10'
+					},{
+						'page': 53,
+						'title': 'Unit - 11'
+					},{
+						'page': 60,
+						'title': 'Unit - 12'
+					}]},'Interactivities': {
+								'icon': 'img/interactivity.png',
+								'menu': 'Interactivities',
+								'link': 'iframe',
+								'data': [
+									{
+								'path': '',
+								'title': 'Activity',
+								'page': 60,
+								'interactivities':[
+									{
+							'path': 'resources/Activity/Unit-1/index.html',
+							'title': 'Unit 1 - Activity',
+							'size': '1200x2000',
+							'page': 5},{
+							'path': 'resources/Activity/Unit-2/index.html',
+							'title': 'Unit 2 - Activity',
+							'size': '1200x2000',
+							'page': 9},{
+							'path': 'resources/Activity/Unit-3/index.html',
+							'title': 'Unit 3 - Activity',
+							'size': '1200x2000',
+							'page': 14},{
+							'path': 'resources/Activity/Unit-4/index.html',
+							'title': 'Unit 4 - Activity',
+							'size': '1200x2000',
+							'page': 18},{
+							'path': 'resources/Activity/Unit-5/index.html',
+							'title': 'Unit 5 - Activity',
+							'size': '1200x2000',
+							'page': 23},{
+							'path': 'resources/Activity/Unit-6/index.html',
+							'title': 'Unit 6 - Activity',
+							'size': '1200x2000',
+							'page': 28},{
+							'path': 'resources/Activity/Unit-7/index.html',
+							'title': 'Unit 7 - Activity',
+							'size': '1200x2000',
+							'page': 33},{
+							'path': 'resources/Activity/Unit-8/index.html',
+							'title': 'Unit 8 - Activity',
+							'size': '1200x2000',
+							'page': 38},{
+							'path': 'resources/Activity/Unit-9/index.html',
+							'title': 'Unit 9 - Activity',
+							'size': '1200x2000',
+							'page': 43},{
+							'path': 'resources/Activity/Unit-10/index.html',
+							'title': 'Unit 10 - Activity',
+							'size': '1200x2000',
+							'page': 49},{
+							'path': 'resources/Activity/Unit-11/index.html',
+							'title': 'Unit 11 - Activity',
+							'size': '1200x2000',
+							'page': 53},{
+							'path': 'resources/Activity/Unit-12/index.html',
+							'title': 'Unit 12 - Activity',
+							'size': '1200x2000',
+							'page': 60}
+									],
+								'size': '1200x2000',
+								}
+								]}
+								,'zother': [
+							
+						]
+						
+					}
+				}
---
+++ data/page.js
@@ -0,0 +1,97 @@
+const PAGES_LINKING = {'5':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-1/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'9':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-2/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'14':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-3/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'18':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-4/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'23':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-5/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'28':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-6/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'33':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-7/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'38':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-8/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'43':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-9/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'49':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-10/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'53':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-11/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}],'60':[{			 
+											x: 67,
+											y: 8,
+											title: 'Activity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Activity/Unit-12/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}]}
